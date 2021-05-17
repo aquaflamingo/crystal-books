abf88fb3c7c551c28404ab055c95ba75e0e21beb
Robert <dev@robertsimoes.org>
2021-05-16 20:05:22 -0700

    Updates main.cr for CLI

--- src/main.cr
+++ src/main.cr
@@ -1,3 +1,19 @@
-require "./account.cr"
+require "option_parser"
 
-puts "Hello Crystal Accounting"
+OptionParser.parse do |parser|
+  parser.banner = "Welcome to CrystalBooks"
+
+  parser.on "-v", "--version", "Show version" do
+		 puts "#{CrystalBooks::VERSION}"
+    exit
+  end
+
+  parser.on "-h", "--help", "Show help" do
+    puts parser
+    exit
+  end
+
+	parser.on "start", "Start CrystalBooks" do
+		 CrystalBooks::App.start(ARGV[0])
+	end
+end
