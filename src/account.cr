4ffc1fcfb89cdf08be4dc5fc2a1397b6cf4c075b
Robert <dev@robertsimoes.org>
2021-05-17 19:34:44 -0700

    Fishes debit credit for account

--- src/account.cr
+++ src/account.cr
@@ -3,11 +3,25 @@ require "./entry.cr"
 class Account
 	 getter id : UUID
 	 getter name : String
-	 getter balance : Float32
 	 getter records : Array(Entry)
 
-	 def initialize(@name, @balance, @records = [] of Entry)
+	 def initialize(@name, initial_balance : Float32, @records = [] of Entry)
 			@id = UUID.random
+
+			# Initial entry
+			records << Entry.new(
+				 Time.utc,
+				 initial_balance,
+				 Entry::Type::DEBIT
+			)
+	 end
+
+	 def balance
+			records.sum do |entry|
+				 amount = entry.amount 
+				 amount *= -1 if entry.type == Entry::Type::CREDIT
+				 amount
+			end
 	 end
 
 	 def credit(time : Time, amount : Float32)
