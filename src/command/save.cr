65515cb83466a56455808f60568d785e9f184210
Robert <dev@robertsimoes.org>
2021-06-06 13:34:25 -0700

    Output on save

--- src/command/save.cr
+++ src/command/save.cr
@@ -3,7 +3,9 @@ require "./base.cr"
 module Command
 	 class Save < Command::Base
 			def run(fname)
+				 print "Saving to #{fname}\n"
 				 File.write(fname, ledger.dump)
+				 print "Done..."
 			end
 	 end
 end
